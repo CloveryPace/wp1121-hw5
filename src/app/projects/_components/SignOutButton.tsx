2948d0371d53a44bf895e0399332a6e19e9ca06b
CloveryPace <128057141+CloveryPace@users.noreply.github.com>
2023-12-20 10:32:04 +0800

    上傳hack2程式

---
+++ src/app/projects/_components/SignOutButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+// TODO: 4. Call the signOut() function when the button is clicked
+// hint: You may want to change the first line of this file
+import { useEffect } from "react";
+
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import { Button } from "@/components/ui/button";
+import { publicEnv } from "@/lib/env/public";
+
+export default function SignOutButton() {
+  const { data: session } = useSession();
+  // const router = useRouter();
+
+  const handleSignOut = async () => {
+    if (session) {
+      signOut({ callbackUrl: publicEnv.NEXT_PUBLIC_BASE_URL });
+    }
+    // router.push("/");
+  };
+  return (
+    <Button
+      data-testid="sign-out-button"
+      variant={"outline"}
+      onClick={handleSignOut}
+    >
+      Sign Out
+    </Button>
+  );
+}
+// TODO: 4. end
